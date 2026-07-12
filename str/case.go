97dfeab89674934a07720b13ba0e665c6b0d4f22
xingchen483 <wang28962023@163.com>
2026-07-12 12:10:36 +0800

    Create case.go

---
+++ str/case.go
@@ -0,0 +1,31 @@
+package str
+
+import (
+	"strings"
+	"unicode"
+)
+
+// CamelToSnake 驼峰转下划线 (例如: UserName -> user_name)
+func CamelToSnake(s string) string {
+	var result []rune
+	for i, r := range s {
+		if i > 0 && unicode.IsUpper(r) {
+			result = append(result, '_')
+		}
+		result = append(result, unicode.ToLower(r))
+	}
+	return string(result)
+}
+
+// SnakeToCamel 下划线转驼峰 (例如: user_name -> UserName)
+func SnakeToCamel(s string) string {
+	parts := strings.Split(s, "_")
+	for i := range parts {
+		if len(parts[i]) > 0 {
+			runes := []rune(parts[i])
+			runes[0] = unicode.ToUpper(runes[0])
+			parts[i] = string(runes)
+		}
+	}
+	return strings.Join(parts, "")
+}
